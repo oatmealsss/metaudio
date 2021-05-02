dfb685fda61e90abe32575a51bc618672688fada
nasso <nassomails@gmail.com>
2021-05-02 22:58:27 +0200

    Add MP3 support

--- src/lib.rs
+++ src/lib.rs
@@ -21,5 +21,17 @@ pub fn read_metadata(buffer: &[u8]) -> Option<AudioMetadata> {
         });
     }
 
+    if let Ok(meta) = mp3_metadata::read_from_slice(buffer) {
+        let sample_rate = meta.frames[0].sampling_freq as u32;
+
+        // im not that proud of this one but that's the best we can do here
+        let sample_length = (meta.duration.as_secs_f32() * sample_rate as f32).round() as u32;
+
+        return Some(AudioMetadata {
+            sample_rate,
+            sample_length,
+        });
+    }
+
     None
 }
